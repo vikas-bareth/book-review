7298ec5f7986e02275fd30ea45cb5a61796b8e75
Vikas <iamvikasbareth@gmail.com>
2025-05-30 00:09:54 +0530

    auth routes added

---
+++ src/routes/auth.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth.controller");
+const userAuth = require("../middlewares/auth");
+
+// Public routes
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
+// Protected routes
+router.get("/me", userAuth, authController.getMe);
+router.post("/logout", userAuth, authController.logout);
+
+module.exports = router;
