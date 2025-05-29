ebf2a55f649749d4d27d1c9a578bbdb48814e968
Vikas <iamvikasbareth@gmail.com>
2025-05-30 00:59:58 +0530

    adding admin role for a user

--- src/models/user.js
+++ src/models/user.js
@@ -30,6 +30,11 @@ const userSchema = new mongoose.Schema(
       minlength: [6, "Password must be at least 6 characters"],
       select: false,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   {
     timestamps: true,
